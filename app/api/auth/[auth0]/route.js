00b9b6e39e50135136726fb37dbd66ab7b093c3b
Yordan Ganev <yordanganew@gmail.com>
2023-07-04 16:24:57 +0300

    Added Auth0

---
+++ app/api/auth/[auth0]/route.js
@@ -0,0 +1,4 @@
+//https://github.com/auth0/nextjs-auth0/tree/beta#app-router
+import { handleAuth } from '@auth0/nextjs-auth0';
+
+export const GET = handleAuth();
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import { UserProvider } from '@auth0/nextjs-auth0/client';
+
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -14,8 +17,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <UserProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </UserProvider>
   )
 }
